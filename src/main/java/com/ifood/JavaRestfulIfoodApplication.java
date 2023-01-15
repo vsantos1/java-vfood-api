d6de63344eae13113407a5e09c52a5a8d7e7d059
vsantos1 <vinicius.shyn2@gmail.com>
2023-01-15 01:02:56 -0300

    setup project

---
+++ src/main/java/com/ifood/JavaRestfulIfoodApplication.java
@@ -0,0 +1,13 @@
+package com.ifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestfulIfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRestfulIfoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ifood/JavaRestfulIfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestfulIfoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
